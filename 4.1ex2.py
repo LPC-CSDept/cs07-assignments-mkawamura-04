a591dc506488e4eae13c5a714bbc826176fd5481
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-02-21 01:59:53 +0000

    Initial Commit

---
+++ 4.1ex2.py
@@ -0,0 +1,9 @@
+for number in [1,2,3,4,5]:
+  usernumber=input('Enter a Number:')
+  user_number=int(usernumber)
+  sum=user_number+user_number
+print(sum)
+
+  
+  
+
---
+++ 4.2ex1.py
@@ -0,0 +1,8 @@
+while x!==0:
+ import random
+number = random.randint(0,100)
+if (number/2)==0:
+  x==0
+else:print(number)
+  
+
