27ceaa32a6fe588d7f038d7e1e70575eff92860f
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-03-20 23:13:42 +0000

    Initial Commit

--- Assignment4.4/4.4ex10.py
+++ Assignment4.4/4.4ex10.py
@@ -1,4 +1,17 @@
 import random
-stop=False
-while stop==False:
+i=0
+sum=0
+s="Numbers: "
+while i<10:
   number = random.randint(0,100)
+  sum=sum+number
+  s=s+str(number)+", "  
+  i=i+1
+  if sum>100:
+    break
+if sum>100:
+  print (s, end='')
+  print ("Sum: %d" % sum)
+else:
+  print ("The total is <=100" )
+    
