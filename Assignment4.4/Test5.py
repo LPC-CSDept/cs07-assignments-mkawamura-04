479afab4cb4e7b53b5a89cc37cbe96d450314b54
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-03-23 06:01:11 +0000

    Question 1 in progress

--- Assignment4.4/Test5.py
+++ Assignment4.4/Test5.py
@@ -5,8 +5,12 @@ for x in range (10):
 print (numbers)
 minVal=min(numbers)
 minValIndex=numbers.index(minVal)
-print (minValIndex)
-print (numbers[5])
 numbers[minValIndex]=numbers[0]
 numbers[0]=minVal
 print (numbers)
+minVal2=min(numbers[1:])
+minVal2Index=numbers[1:].index(minVal2)
+numbers[minVal2Index]=numbers[1]
+numbers[1]=minVal2
+print(numbers)
+
