0b6ba7cadea83b5c873e72d8cd00b82d3b521353
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-03-23 06:23:10 +0000

    Question 2 in progress

---
+++ Assignment4.4/Test52.py
@@ -0,0 +1,6 @@
+n=input("Number of Students: ")
+m=input('Number of Scores: ')
+names=input('Names of Students')
+for i in range (m):
+  Student 1 scores=input('Student 1 Scores: ')
+scores=[score1, score2]
