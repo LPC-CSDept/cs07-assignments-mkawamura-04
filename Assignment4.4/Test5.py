264c89e6e2ed45c4ed154f566c16594a5567c6e4
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-03-23 06:02:44 +0000

    Question 1 Final Answer

--- Assignment4.4/Test5.py
+++ Assignment4.4/Test5.py
@@ -9,7 +9,7 @@ numbers[minValIndex]=numbers[0]
 numbers[0]=minVal
 print (numbers)
 minVal2=min(numbers[1:])
-minVal2Index=numbers[1:].index(minVal2)
+minVal2Index=numbers[1:].index(minVal2)+1
 numbers[minVal2Index]=numbers[1]
 numbers[1]=minVal2
 print(numbers)
