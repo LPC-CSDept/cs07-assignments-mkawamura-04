a07599fdf60422e88dbc78225992c356429b75cf
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-04-06 03:30:43 +0000

    Changed "list" variable to "list1" so as not to override the list command

--- Assignment7/7.2.py
+++ Assignment7/7.2.py
@@ -1,12 +1,12 @@
 User_input=input('User Input: ')
 s=User_input.split(' ')
-list=[]
+list1=[]
 for i in range(int(s[0])):
   user_input=int(s[i+1])
-  list.append(user_input)
-Average=sum(list)/len(list)
-for i in range(len(list)):
-  Difference=abs(Average-list[i])
+  list1.append(user_input)
+Average=sum(list1)/len(list1)
+for i in range(len(list1)):
+  Difference=abs(Average-list1[i])
   print (Difference, end = ' ')
   
   
