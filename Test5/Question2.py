287c1f1d17d2fbe3a84d20b80ffbbb63cdd8dbf4
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-04-09 22:42:04 +0000

    Final Commit

--- Test5/Question2.py
+++ Test5/Question2.py
@@ -4,17 +4,21 @@ names=input('Names of Students: ')
 nameList=names.split(' ')
 scoreList=[]
 allScores=[]
-
 for i in range (n):
   scores=input('Score: ')
   scoreList=scores.split(' ')
   for x in range (len(scoreList)):
       scoreList[x]=int(scoreList[x])
   allScores.append(scoreList)
-print(allScores)
 for i in range (n):
   row_sum=0
   for j in range (m):
     row_sum=row_sum+allScores[i][j]
   average=row_sum/m
-  print ('Name: {0} Sum: {1} Average:     {2}'.format(nameList[i],row_sum,average))
+  print ('Name:{0}, Sum:{1}, Average:   {2}'.format(nameList[i],row_sum,average))
+for j in range (m):
+  column_sum=0
+  for i in range (n):
+    column_sum=column_sum+allScores[i][j]
+  average=column_sum/n
+  print ('Subject Number:{0}, Subject Sum: {1}, Subject Average:{2}'.format(j+1,column_sum,average))
