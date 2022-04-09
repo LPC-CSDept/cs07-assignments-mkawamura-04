556dd31e401ecfc934a7d6aabe2c9414912e0dd5
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-04-09 19:14:49 +0000

    Final Commit

--- Quiz4/Question2.py
+++ Quiz4/Question2.py
@@ -1,9 +1,9 @@
-stop=False
 i=0
-while stop==False:
-  user_input=input('Input String: ')
-  if user_input==stop:
+while True:
+  user_input=input('Input Word: ')
+  if user_input=='stop':
     break
   if 'p' in user_input:
     i=i+1
+print (i)
     
