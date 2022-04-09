0b66791bfc01baad4084b22ef74657278fb8eb2a
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-04-09 18:27:03 +0000

    Commit 3

--- Quiz4/Question1.py
+++ Quiz4/Question1.py
@@ -1,14 +1,19 @@
 stop=False
 user_input=input('Input Numbers: ')
 s=user_input.split(' ')
+numbers=[]
 for i in range (len(s)):
-  numbers[i]=int(s[i])
+  numbers.append(int(s[i]))
 while stop==False:
   for i in range(len(numbers)):
-    if numbers[i] % 2=0 and numbers[i+1] % 2=0:
+    if numbers[i] % 2==0 and numbers[i+1] % 2==0:
       print(numbers[i])
       print(numbers[i+1])
       break
-else:
+      stop=True
+    else:
+      continue
   print('No Consectutive Even Numbers')
+  stop=True
+      
     
