701efdb41cf3e72d73a99a3fe1579ac21a579e16
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-04-09 22:32:29 +0000

    Commit 3

--- Test5/Question2.py
+++ Test5/Question2.py
@@ -4,6 +4,7 @@ names=input('Names of Students: ')
 nameList=names.split(' ')
 scoreList=[]
 allScores=[]
+
 for i in range (n):
   scores=input('Score: ')
   scoreList=scores.split(' ')
@@ -11,3 +12,9 @@ for i in range (n):
       scoreList[x]=int(scoreList[x])
   allScores.append(scoreList)
 print(allScores)
+for i in range (n):
+  row_sum=0
+  for j in range (m):
+    row_sum=row_sum+allScores[i][j]
+  average=row_sum/m
+  print ('Name: {0} Sum: {1} Average:     {2}'.format(nameList[i],row_sum,average))
