57b5449c7acf016974dce0bb6ae8047d872c9c52
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-21 05:11:22 +0000

    Commit #2

--- Quiz6/Question2.py
+++ Quiz6/Question2.py
@@ -1,18 +1,18 @@
-getEvens=lambda n: True if n%2==0 else False:
+getEvens=lambda n: True if n%2==0 else False
 def getMerged (list1,list2):
-  list1=[1,2,3,4]
-  list2=[3,4,5,6]
+  list3=[]
+  list4=[]
   for i in range (len(list1)):
     n=list1[i]
     if not getEvens (n):
-      list1.append(n)
-  for i in range(len(list2))
+      list3.append(n)
+  for i in range(len(list2)):
     n=list2[i]
     if not getEvens (n):
-      list2.append(n)
-  finalList=list1+list2
+      list4.append(n)
+  finalList=list3+list4
   return (finalList)
-#list1=[1,2,3,4]
-#list2=[5,6,7,8]
+list1=[1,2,3,4]
+list2=[5,6,7,8]
 result=getMerged (list1,list2)
 print(result)
