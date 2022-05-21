42ca91e7e56445984bfd4c86d230c4004269901f
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-21 19:02:35 +0000

    Final Commit for parts 1 and 2 w/o printing initial lists

--- Quiz7/Question1.py
+++ Quiz7/Question1.py
@@ -1,25 +1,33 @@
 import xlrd
+
 workbook = xlrd.open_workbook('student.xls')
 sheetNames = workbook.sheet_names()
 sheetByIndex = workbook.sheet_by_index(0)
+
 header = sheetByIndex.row_values(0)
 header = list(filter(lambda cell: cell != '', header))
-print(header)
+
 id = sheetByIndex.col_values(0, start_rowx=1)
-print(id)
+
 name = sheetByIndex.col_values(1, start_rowx=1)
-print(name)
-num_scores = sheetByIndex.ncols - 2
+
+num_scores = sheetByIndex.nrows - 1
 scores = []
 for i in range(num_scores):
-    score = sheetByIndex.row_values(i+2, start_colx=2)
+    score = sheetByIndex.row_values(i+1, start_colx=2)
     scores.append(score)
-print(scores)
+
+dictList = []
+for pid in range(len(name)):
+    record = []
+    record.append(id[pid])
+    record.append(name[pid])
+    record.append(scores[pid])
+    d = dict(zip(header, record))
+    dictList.append(d)
+print(dictList)
 
 
-dictionary=dict(zip(header,record))
-for zipval dictionary 
-print (dictionary)
 """highScore = lambda l: filter(lambda n: n>280, l)
 result=highScore(totalscore)
 print (total score data)
