4942632794eb98e016e8dd7d15100a1a6938c0e2
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-21 05:42:23 +0000

    Added yield

--- Quiz6/Question2.py
+++ Quiz6/Question2.py
@@ -1,18 +1,15 @@
-getEvens=lambda n: True if n%2==0 else False
+isEvens=lambda n: True if n%2==0 else False
 def getMerged (list1,list2):
   list3=[]
   list4=[]
   for i in range (len(list1)):
     n=list1[i]
-    if not getEvens (n):
+    if not isEvens (n):
       list3.append(n)
   for i in range(len(list2)):
     n=list2[i]
-    if not getEvens (n):
+    if not isEvens (n):
       list4.append(n)
-  finalList=list3+list4
-  return (finalList)
-list1=[1,2,3,4]
-list2=[5,6,7,8]
-result=getMerged (list1,list2)
-print(result)
+  yield (list3+list4)
+result=getMerged([1,2,3,4],[5,6,7,8])
+print (next(result))
