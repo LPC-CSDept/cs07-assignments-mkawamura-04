566cf5674841215e16c8d3d294a32b6cf96437e7
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-21 18:28:28 +0000

    Commit #2

--- Quiz7/Question1.py
+++ Quiz7/Question1.py
@@ -2,9 +2,26 @@ import xlrd
 workbook = xlrd.open_workbook('student.xls')
 sheetNames = workbook.sheet_names()
 sheetByIndex = workbook.sheet_by_index(0)
-firstColumn = sheetByIndex.col_values(0)
-print(firstColumn)
-secondColumn = sheetByIndex.col_values(1)
-print(secondColumn)
-print(sheetByIndex.col_values(2))
+header = sheetByIndex.row_values(0)
+header = list(filter(lambda cell: cell != '', header))
+print(header)
+id = sheetByIndex.col_values(0, start_rowx=1)
+print(id)
+name = sheetByIndex.col_values(1, start_rowx=1)
+print(name)
+num_scores = sheetByIndex.ncols - 2
+scores = []
+for i in range(num_scores):
+    score = sheetByIndex.row_values(i+2, start_colx=2)
+    scores.append(score)
+print(scores)
 
+
+dictionary=dict(zip(header,record))
+for zipval dictionary 
+print (dictionary)
+"""highScore = lambda l: filter(lambda n: n>280, l)
+result=highScore(totalscore)
+print (total score data)
+from functool import reduce
+functools.reduce(lambda sheetByIndex: id,name,total score,)"""
