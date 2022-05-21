ae9df857e74d33a3eebef6c0ca9db107f67987ea
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-21 07:02:21 +0000

    Final Commit

--- Quiz6/Question1.py
+++ Quiz6/Question1.py
@@ -9,19 +9,16 @@ def add_total_score(data):
         for i in range(SCORE_BEGIN_INDEX, SCORE_END_INDEX):
             total = total + r[i]
         r.append(total)
-
 def read_input(n):
     data = []
     for i in range(n):
         user_input = input("Student Record (ID, Name, Scores): ").split()
-
-        for j in range(SCORE_BEGIN_INDEX,SCORE_END_INDEX):
+    for j in range(SCORE_BEGIN_INDEX,SCORE_END_INDEX):
             user_input[j] = int(user_input[j])
 
         data.append(user_input)
 
     return data
-
 def print_all(data):
     for r in data:
         for i in range(0, SCORE_END_INDEX):
@@ -29,7 +26,6 @@ def print_all(data):
         total = r[TOTAL_SCORE_INDEX]
         avg = total /NUM_SCORE
         print("Total: {0} Avg: {1}".format(total, avg))
-
 def find_highest_score(data):
     max_score_idx = 0
     for idx, r in enumerate(data):
@@ -40,9 +36,7 @@ def find_highest_score(data):
             max_score_idx = idx
         else:
             pass
-
-    print(data[max_score_idx][0], data[max_score_idx][1])
-
+  print(data[max_score_idx][0], data[max_score_idx][1])
 def find_lowest_score(data, score_idx):
     in_score_idx = score_idx + SCORE_BEGIN_INDEX - 1
     min_score_idx = 0
@@ -54,12 +48,9 @@ def find_lowest_score(data, score_idx):
             min_score_idx = idx
         else:
             pass
-
     print(data[min_score_idx][0], data[min_score_idx][1])
-
 data = read_input(NUM_RECORDS)
 add_total_score(data)
-
 print_all(data)
 find_highest_score(data)
 find_lowest_score(data, 1)
--- Quiz6/Question2.py
+++ Quiz6/Question2.py
@@ -1,16 +1,17 @@
-isEvens=lambda n: True if n%2==0 else False
-def getMerged (list1,list2):
-  list3=[]
-  list4=[]
-  getEvens=lambda n: if not isEvens (n): list3.append(n)
-  for i in range (len(list1)):
-    n=list1[i]
-    if not isEvens (n):
-      list3.append(n)
-  for i in range(len(list2)):
-    n=list2[i]
-    if not isEvens (n):
-      list4.append(n)
-  yield (list3+list4)
-result=getMerged([1,2,3,4],[5,6,7,8])
-print (next(result))
+getEvens = lambda l: filter(lambda n: n%2 == 0, l)
+def getMerged(list1, list2):
+    e1 = getEvens(list1)
+    e2 = getEvens(list2)
+
+    for i in e1:
+        yield i
+    for i in e2:
+        yield i
+
+list1 = [1,2,3,4]
+list2 = [5,6,7,8]
+print(list1)
+print(list2)
+
+for i in getMerged(list1, list2):
+    print(i, end=' ')
