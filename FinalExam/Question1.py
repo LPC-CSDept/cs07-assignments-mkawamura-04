b8a0ab5b1c0a46dd6a9caa4bb2562ba0114caa79
minhnhak <81109719+mkawamura-04@users.noreply.github.com>
2022-05-27 00:16:15 +0000

    Commit 2

--- FinalExam/Question1.py
+++ FinalExam/Question1.py
@@ -1,6 +1,27 @@
+from functools import reduce
+
+toInt=lambda x:int(x.replace(",",""))
+
 def makeLists(fname,lists):
     f=open(fname)
     for line in f.readlines():
         record=line.split()
         for i in range(0,4):
             lists[i].append(record[i+1])
+          
+def makeDict(data,keyList,state,nameList,gender,numberList):
+    for i in range(len(nameList)):
+        valueList=[state,nameList[i],gender,numberList[i]]
+        data.append(dict(zip(keyList,valueList)))
+
+def buildData(data):
+    camale=[]
+    cafemale=[]
+    camalenum=[]
+    cafemalenum=[]
+    flmale=[]
+    flfemale=[]
+    flmalenum=[]
+    flfemalenum=[]
+    makeLists("ca2021.txt",[camale,camalenum,cafemale,cafemalenum])
+    makeLists("fl2021.txt",[flmale,flmalenum,flfemale,flfemalenum])
